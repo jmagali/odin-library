6da2a289649890027204769972fa0d8238c8a1c4
JM <jmagali.dev@gmail.com>
2025-08-25 01:03:42 -0400

    Remove ternary logic from book constructor

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 let library = []
 
-function Book (id, title, author, pages, readPages, status, notes, rating) {
+function Book (id, title, author, pages, readPages, completed, notes, rating) {
     this.id = id,
     this.title = title,
     this.author = author,
     this.pages = pages,
-
-    // If all pages are read, status is true
-    this.status = pages === readPages ? true : status,
-
-    // If the book is completed, the read pages is the number of pages in the book
-    this.readPages = status ? pages : readPages,
-
+    this.readPages = readPages,
+    this.completed = completed,
     this.notes = notes,
     this.rating = rating
 }
