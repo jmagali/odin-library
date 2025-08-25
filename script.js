6c9fb2bfb27780ee09f3a3a9a24400f1b4164c86
JM <jmagali.dev@gmail.com>
2025-08-25 01:04:21 -0400

    Swap commas for semi-colons in book constructor

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let library = []
 
 function Book (id, title, author, pages, readPages, completed, notes, rating) {
-    this.id = id,
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.readPages = readPages,
-    this.completed = completed,
-    this.notes = notes,
-    this.rating = rating
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readPages = readPages;
+    this.completed = completed;
+    this.notes = notes;
+    this.rating = rating;
 }
